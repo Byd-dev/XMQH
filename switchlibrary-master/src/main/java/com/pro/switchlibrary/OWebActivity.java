57af25724a49206103307b8e805b9463cbbf213e
aaron <aaronstepbystep@gmail.com>
2019-09-12 16:20:43 +0800

    删除了无效提示

--- switchlibrary-master/src/main/java/com/pro/switchlibrary/OWebActivity.java
+++ switchlibrary-master/src/main/java/com/pro/switchlibrary/OWebActivity.java
@@ -396,8 +396,6 @@ public class OWebActivity extends BaseActivity {
         StatusBarUtil.setRootViewFitsSystemWindows(this, true);
         //设置状态栏透明
         StatusBarUtil.setTranslucentStatus(this);
-        //一般的手机的状态栏文字和图标都是白色的, 可如果你的应用也是纯白色的, 或导致状态栏文字看不清
-        //所以如果你是这种情况,请使用以下代码, 设置状态使用深色文字图标风格, 否则你可以选择性注释掉这个if内容
         if (!StatusBarUtil.setStatusBarDarkTheme(this, true)) {
             //如果不支持设置深色风格 为了兼容总不能让状态栏白白的看不清, 于是设置一个状态栏颜色为半透明,
             //这样半透明+白=灰, 状态栏的文字能看得清
@@ -462,7 +460,6 @@ public class OWebActivity extends BaseActivity {
 
     // 调起支付宝并跳转到指定页面
     private void startAlipayActivity(String url) {
-        Log.d("print", "startAlipayActivity:支付宝页面626: " + url);
         Intent intent;
         try {
             intent = Intent.parseUri(url,
